2a981d3942d33d3b53ec90e0a2a26c11b4759c4e
Musa-1995 <72025497+Musa-1995@users.noreply.github.com>
2020-10-02 12:51:19 +0100

    submitting

---
+++ app.js
@@ -0,0 +1,72 @@
+
+function diplay() {
+    var x = document.getElementById("calculator");
+      x.style.display = "flex";
+  }
+
+//   function calculate(){
+//       var i = "";
+//       i = document.getElementById("name").value;
+//       var j = 0;
+//       j = document.getElementById("year").value;
+
+//       j *= 365;
+      function ageCalculate(){
+        //document.getElementById('age').InnerHtml
+      // document.getElementById("demo").InnerHtml=4+5;
+       //document.write="Hi";
+       var i = "";
+         i = document.getElementById("name").value;
+       var birthDate =document.getElementById('birth_date').value;
+    
+    //if(birthDate==""){
+      //  alert("Choose correct birthdate.")
+    //}else{
+      //  alert(birthDate);
+      //  alert(Date());
+        var d = new Date(birthDate);
+    
+       // document.getElementById("age").innerHTML = d;
+    
+    //}
+    
+            var mdate = birthDate.toString();
+            var yearThen = parseInt(mdate.substring(0,4), 10);
+            var monthThen = parseInt(mdate.substring(5,7), 10);
+            var dayThen = parseInt(mdate.substring(8,10), 10);
+            
+            var today = new Date();
+            var birthday = new Date(yearThen, monthThen-1, dayThen);
+         //   alert(today.valueOf() + " " + birthday.valueOf());
+            var differenceInMilisecond = today.valueOf() - birthday.valueOf();
+          //  alert(differenceInMilisecond);
+            
+            var year_age = Math.floor(differenceInMilisecond / 31536000000);
+            var day_age = Math.floor((differenceInMilisecond % 31536000000) / 86400000);
+            
+            if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {
+                alert("Happy B'day!!!");
+            }
+            
+            var month_age = Math.floor(day_age/30);
+            
+            day_age = day_age % 30;
+            
+            var tMnt= (month_age + (year_age*12));
+            var tDays =(tMnt*30) + day_age;
+            
+            if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
+                document.getElementById("age").innerHTML = ("Invalid birthday - Please try again!");
+            }
+            else {
+      document.getElementById("age").innerHTML = i+" You have live for "+year_age + " years " + month_age + " months " + day_age + " days"
+            + "<br/> or <br/> " 
+            + tMnt + " months " + day_age + " days" 
+            + "<br/> or <br/>" 
+            +  tDays + " days" 
+            + "<br/> or <br/>" 
+            +  tDays*24 + " hours" 
+            + "<br/> or <br/>" 
+            +  tDays*24*3600 + " seconds" 
+            + "<br/> or <br/>" 
+            +  tDays*24*3600*1000 + " miliseconds" ;}}
